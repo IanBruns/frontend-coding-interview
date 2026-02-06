4777c6596c9b1e3aef2a5ea71859e06cf58f0f98
Ian <ian@rbruns.com>
2026-02-05 18:08:08 -0700

    feat: style images page

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
+import Link from "next/link"
+import { FaStar } from "react-icons/fa"
 
 import { getImages } from "./api"
-import Link from "next/link"
 import AuthCheck from "./AuthCheck"
 
 type Photo = {
@@ -20,22 +21,53 @@ export default async function ImagesPage() {
   const imageData = await getImages()
 
   return (
-    <div>
+    <div className="p-4 overflow-auto lg:flex lg:items-center lg:flex-col">
       <AuthCheck />
-      <h1>All Photos</h1>
+      <Image
+        className="mb-3"
+        src="/logo.svg"
+        alt="CI Logo"
+        width={75}
+        height={75}
+      />
+      <h1 className="font-bold text-xl font-sans mb-3">All Photos</h1>
       {imageData?.photos.map((photo: Photo) => (
-        <div key={photo.id}>
-          <p>{photo.liked.toString()}</p>
-          <Image
-            src={photo.src.medium}
-            alt={photo.alt}
-            height={75}
-            width={75}
-          />
-          <p>{photo.photographer}</p>
-          <Link href={photo.photographer_url}>Portfolio</Link>
-          <p>{photo.alt}</p>
-          <p>{photo.avg_color}</p>
+        <div key={photo.id} className="flex flex-row mb-2 h-full items-center">
+          <FaStar color={photo.liked ? "#FFD600" : "#9CA3AF"} />
+          <div className="flex flex-row h-full">
+            <Image
+              className="w-[75px] h-[75px] aspect-square rounded-lg mr-3"
+              width={75}
+              height={75}
+              src={photo.src.medium}
+              alt={photo.alt}
+            />
+            <div className="flex flex-col w-[219px] h-full">
+              <p className="font-bold text-sm">{photo.photographer}</p>
+              <p className="font-normal text-sm">{photo.alt}</p>
+              <div className="flex items-center">
+                <p
+                  className="font-normal text-sm mr-3"
+                  style={{ color: photo.avg_color }}
+                >
+                  {photo.avg_color}
+                </p>
+                <div
+                  className="h-3 w-3"
+                  style={{ backgroundColor: photo.avg_color }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <Image src="/links.svg" alt="CI Logo" width={12} height={12} />
+            <Link
+              className="font-normal text-sm text-[#0075EB] hover:underline hover:cursor-pointer"
+              href={photo.photographer_url}
+            >
+              Portfolio
+            </Link>
+          </div>
         </div>
       ))}
     </div>
